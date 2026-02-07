abb37e69f76224df3721d06f56d199f65e5280c8
Linkshimcat <yunjae14278@gmail.com>
2026-02-08 00:11:10 +0900

    Initial clean commit

---
+++ season/script.js
@@ -0,0 +1,22 @@
+/*
+const teenipings = [
+{
+    name: "떠벌핑",
+    season: 1,
+    img: "teenieping/1기/Tweetping.png",
+    desc: "행복을 담당하는 티니핑 ✨"
+  }
+];
+
+const container = document.getElementById("cardContainer");
+
+teenipings.forEach(t => {
+  container.innerHTML += `
+    <div class="card">
+      <img src="${t.img}">
+      <h2>${t.name}</h2>
+      <p>${t.desc}</p>
+    </div>
+  `;
+});
+*/
